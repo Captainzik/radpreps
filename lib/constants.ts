45c7bbc29ff784e552c665c6a9d260daeccf198f
Captainzik <j.sparrowworld@gmail.com>
2026-04-12 23:33:15 -0600

    name changed to radpreps

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN ||
   'ARDMS, CAMRT, ARRT, Sonography Canada, CCI, CME. Exam Prep for Ultrasound & Radiography'
 
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'FlyPreps'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'RadPreps'
 
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
